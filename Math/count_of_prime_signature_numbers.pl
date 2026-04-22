439869dbea57ddbf26141712f8592509cfcba86b
trizen <trizenx@tutanota.com>
2026-04-22 22:37:20 +0300

    new file:   Math/count_of_prime_signature_numbers.pl new file:   Math/prime_signature_numbers_in_range.pl

---
+++ Math/count_of_prime_signature_numbers.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 22 April 2026
+# https://github.com/trizen
+
+# Count the number of k-omega prime numbers <= n that have a given prime signature.
+
+# See also:
+#   https://en.wikipedia.org/wiki/Almost_prime
+#   https://en.wikipedia.org/wiki/Prime_signature
+
+use 5.036;
+use ntheory 0.74 qw(:all);
+
+sub count_prime_signature_numbers($n, $prime_signature) {
+
+    my $k = scalar(@$prime_signature);
+
+    # Handle empty prime signature
+    if ($k == 0) {
+        return 1 if (1 <= $n);
+        return 0;
+    }
+
+    my $count = 0;
+
+    my $generate = sub ($m, $lo, $k, $P, $j = 0) {
+
+        my $e  = $P->[$k - 1];
+        my $hi = rootint(divint($n, $m), ($k > $e ? $k : $e));
+
+        if ($k == 1) {
+            $count += prime_count($hi) - $j;
+            return;
+        }
+
+        foreach my $p (@{primes($lo, $hi)}) {
+            my $t = mulint($m, powint($p, $e));
+            my $u = rootint(divint($n, $t), $P->[$k - 2]);
+            last if ($p + 1 > $u);
+            __SUB__->($t, $p + 1, $k - 1, $P, ++$j);
+        }
+    };
+
+    my %seen;
+    forperm {
+        my @perm = @{$prime_signature}[@_];
+        if (!$seen{join(' ', @perm)}++) {
+            $generate->(1, 2, scalar(@perm), \@perm);
+        }
+    } $k;
+
+    return $count;
+}
+
+sub count_prime_signature_numbers_in_range($A, $B, $signature) {
+
+    my $term_1 = count_prime_signature_numbers($A - 1, $signature);
+    my $term_2 = count_prime_signature_numbers($B,     $signature);
+
+    $term_2 - $term_1;
+}
+
+#
+## Example
+#
+sub A395379($n) {
+
+    my $A = powint((nth_prime($n - 1) || 1), 7);
+    my $B = powint(nth_prime($n),            7) - 1;
+
+    my $term_1 = count_prime_signature_numbers_in_range($A, $B, [7]);
+    my $term_2 = count_prime_signature_numbers_in_range($A, $B, [3, 1]);
+    my $term_3 = count_prime_signature_numbers_in_range($A, $B, [1, 1, 1]);
+
+    $term_1 + $term_2 + $term_3;
+}
+
+join(' ', map { A395379($_) } 1 .. 9) eq join(' ', 15, 408, 16838, 167649, 4140037, 9474308, 74874018, 102945521, 527810589)
+  or die "error";
+
+my $prime_signature = [3, 2, 2];
+my $n               = 10000;
+
+count_prime_signature_numbers($n, $prime_signature) == 7                or die "error";
+count_prime_signature_numbers_in_range(2e3, 1e4, $prime_signature) == 6 or die "error";
---
+++ Math/prime_signature_numbers_in_range.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+
+# Daniel "Trizen" Șuteu
+# Date: 22 April 2026
+# https://github.com/trizen
+
+# Generate all the k-omega prime numbers <= n that have a given prime signature.
+
+# See also:
+#   https://en.wikipedia.org/wiki/Almost_prime
+#   https://en.wikipedia.org/wiki/Prime_signature
+
+use 5.036;
+use ntheory 0.74 qw(:all);
+
+sub rootint_ceil($n, $k) {
+    if (is_power($n, $k)) {
+        return rootint($n, $k);
+    }
+    return 1 + rootint($n, $k);
+}
+
+sub prime_signature_numbers_in_range($A, $B, $prime_signature) {
+
+    my @list;
+    my $k = scalar(@$prime_signature);
+
+    # Handle empty prime signature
+    if ($k == 0) {
+        push(@list, 1) if ($A <= 1 and 1 <= $B);
+        return @list;
+    }
+
+    $A = vecmax(pn_primorial($k), $A);
+
+    my $generate = sub ($m, $lo, $k, $P) {
+
+        my $e  = $P->[$k - 1];
+        my $hi = rootint(divint($B, $m), ($k > $e ? $k : $e));
+
+        if ($k == 1) {
+
+            $lo = vecmax($lo, rootint_ceil(cdivint($A, $m), $e));
+
+            if ($lo > $hi) {
+                return;
+            }
+
+            foreach my $p (@{primes($lo, $hi)}) {
+                push @list, mulint($m, powint($p, $e));
+            }
+
+            return;
+        }
+
+        foreach my $p (@{primes($lo, $hi)}) {
+            my $t = mulint($m, powint($p, $e));
+            my $u = rootint(divint($B, $t), $P->[$k - 2]);
+            last if ($p + 1 > $u);
+            __SUB__->($t, $p + 1, $k - 1, $P);
+        }
+    };
+
+    my %seen;
+    forperm {
+        my @perm = @{$prime_signature}[@_];
+        if (!$seen{join(' ', @perm)}++) {
+            $generate->(1, 2, scalar(@perm), \@perm);
+        }
+    } $k;
+
+    sort { $a <=> $b } @list;
+}
+
+my $prime_signature = [3, 2, 2];
+
+my $A = 2000;
+my $B = 10000;
+
+my @arr = prime_signature_numbers_in_range($A, $B, $prime_signature);
+say "@arr";
+
+# Brute-force check
+my @bf = grep { join(' ', prime_signature($_)) eq join(' ', @$prime_signature) } vecmax(pn_primorial(scalar(@$prime_signature)), $A) .. $B;
+say "@bf";
+
+"@arr" eq "@bf" or die "error";
