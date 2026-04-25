6499f520a61858b4455dd52d00589bc3b5db3728
trizen <trizenx@tutanota.com>
2026-04-25 21:31:16 +0300

    modified:   Math/count_of_prime_signature_numbers.pl -- much faster

--- Math/count_of_prime_signature_numbers.pl
+++ Math/count_of_prime_signature_numbers.pl
@@ -6,10 +6,6 @@
 
 # Count the number of k-omega numbers <= n that have a given prime signature.
 
-# See also:
-#   https://en.wikipedia.org/wiki/Almost_prime
-#   https://en.wikipedia.org/wiki/Prime_signature
-
 use 5.036;
 use ntheory 0.74 qw(:all);
 
@@ -17,7 +13,6 @@ sub count_prime_signature_numbers($n, $prime_signature) {
 
     my $k = scalar(@$prime_signature);
 
-    # Handle empty prime signature
     if ($k == 0) {
         return 1 if (1 <= $n);
         return 0;
@@ -27,38 +22,45 @@ sub count_prime_signature_numbers($n, $prime_signature) {
 
     my $generate = sub ($m, $lo, $k, $P, $j = 0) {
 
-        my $e  = $P->[$k - 1];
-        my $hi = rootint(divint($n, $m), ($k > $e ? $k : $e));
+        my $e = $P->[$k - 1];
 
-        # Base case for k = 1 (handles initial k=1 inputs)
+        # Aggressive Math Pruning: Sum of all remaining exponents
+        my $sum_e = 0;
+        $sum_e += $_ for @{$P}[0 .. $k - 1];
+
+        my $hi = rootint(divint($n, $m), $sum_e);
+
         if ($k == 1) {
             $count += prime_count($hi) - $j;
             return;
         }
 
-        # Intercept recursion at k = 2 for performance
+        my $primes = primes($lo, $hi);
+        my $e2     = $P->[$k - 2];
+
         if ($k == 2) {
-            my $e2 = $P->[0];
-            for (my $p = $lo ; $p <= $hi ;) {
-                my $r = next_prime($p);
+            foreach my $p (@$primes) {
                 my $t = mulint($m, powint($p, $e));
                 my $u = rootint(divint($n, $t), $e2);
-                last if ($r > $u);
+
+                # If current prime >= limit for next prime, break.
+                last if ($p >= $u);
+
                 $count += prime_count($u) - ++$j;
-                $p = $r;
             }
             return;
         }
 
         # General recursive case for k > 2
-        for (my $p = $lo ; $p <= $hi ;) {
-            my $r  = next_prime($p);
-            my $t  = mulint($m, powint($p, $e));
-            my $e2 = $P->[$k - 1];
-            my $u  = rootint(divint($n, $t), ($k - 1 > $e2 ? $k - 1 : $e2));
-            last if ($r > $u);
-            __SUB__->($t, $r, $k - 1, $P, ++$j);
-            $p = $r;
+        my $sum_e_next = $sum_e - $e;
+
+        foreach my $p (@$primes) {
+            my $t = mulint($m, powint($p, $e));
+            my $u = rootint(divint($n, $t), $sum_e_next);
+
+            last if ($p >= $u);
+
+            __SUB__->($t, $p + 1, $k - 1, $P, ++$j);
         }
     };
 
@@ -74,10 +76,8 @@ sub count_prime_signature_numbers($n, $prime_signature) {
 }
 
 sub count_prime_signature_numbers_in_range($A, $B, $signature) {
-
     my $term_1 = count_prime_signature_numbers($A - 1, $signature);
     my $term_2 = count_prime_signature_numbers($B,     $signature);
-
     $term_2 - $term_1;
 }
 
@@ -85,7 +85,6 @@ sub count_prime_signature_numbers_in_range($A, $B, $signature) {
 ## Example
 #
 sub A395379($n) {
-
     my $A = powint((nth_prime($n - 1) || 1), 7);
     my $B = powint(nth_prime($n),            7) - 1;
 
